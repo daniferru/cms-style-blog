ce0293e4e42185c28ba22cf45f5d6deeaaaa6a9a
Daniella <danferrufino@icloud.com>
2023-06-06 17:15:58 -0400

    added handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,13 @@
 async function loginHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username-login').value.trim();
     const email =document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#pw-login').value.trim();
 
-    if (username && email && password) {
+    if (email && password) {
         const response = await fetch('/api/user', {
             method: 'POST',
             body: JSON.stringify({
-                username,
                 email,
                 password
             }),
@@ -18,8 +16,6 @@ async function loginHandler(event) {
 
         //check response status
         if (response.ok) {
-            console.log('success');
-            document.log('success');
             document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
@@ -30,20 +26,23 @@ async function loginHandler(event) {
 async function signupHandler(event) {
     event.preventDefault();
 
+    const username = document.querySelector('#create-username').value.trim();
     const email = document.querySelector('#create-email').value.trim();
     const password = document.querySelector('#create-pw').value.trim();
 
-    if (email && password) {
+    if (username && email && password) {
         const response = await fetch('/api/users/login', {
             method: 'POST',
             body: JSON.stringify({
-                emial,
+                username,
+                email,
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
         });
 
-        if (response.ok) {
+        if (response.ok) {;
+            console.log('sucess')
             document.location.replace('/dashboard');
         } else {
             alert(response.statusType);
